bdb913cb93f159d01550b85995c616e623f4b8b8
Rowa <jrowah@gmail.com>
2022-04-28 23:57:58 +0300

    Alternative solution

--- 2022-0417/wars28.js
+++ 2022-0417/wars28.js
@@ -30,3 +30,35 @@ function countPositivesSumNegatives(input) {
     
     return [positive, negative];
 }
+
+//Alternative solution 
+function countPositivesSumNegatives(input) {
+    var newArr = [];
+    var positiveNumber = 0;
+    var negativeNumber = 0;
+    
+    // Validate Input
+    if (input === null || input.length === 0)
+      return newArr;
+    
+    // Loop through array of Numbers 
+    for (var i = 0; i < input.length; i++) {
+      if (input[i] == 0)
+       continue;
+    
+      // Count positives
+      else if (input[i] > 0) 
+        positiveNumber++;     
+          
+      // Sum negatives
+      else if (input[i] < 0) 
+        negativeNumber += input[i];
+      
+    }
+    
+    // Prepare Output
+    newArr.push(positiveNumber);
+    newArr.push(negativeNumber);
+    
+    return newArr;
+  }
