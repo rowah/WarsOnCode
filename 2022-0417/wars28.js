6ea8ecad95e958586740081d51de35b670bb588b
Rowa <jrowah@gmail.com>
2022-04-29 00:43:53 +0300

    converting time past midnight into millisecs...*60,000

--- 2022-0417/wars28.js
+++ 2022-0417/wars28.js
@@ -62,3 +62,12 @@ function countPositivesSumNegatives(input) {
     
     return newArr;
   }
+
+  //Clock shows h hours, m minutes and s seconds after midnight.
+
+//Your task is to write a function which returns the time since midnight in milliseconds.
+function past(h, m, s){
+    //#Happy Coding! ^_^
+    return ((h * 60 + m + s/60)*60000)
+    
+  }
