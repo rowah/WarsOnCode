b5b9bf56b8cc93737539f828480ada186eae769b
Rowa <jrowah@gmail.com>
2022-04-30 01:31:18 +0300

    finding the area of a square given the length of its acr

--- 2022-0417/wars28.js
+++ 2022-0417/wars28.js
@@ -112,3 +112,11 @@ function setAlarm(employed, vacation){
 function find_average(array) {
     return (array.length < 1 || array == undefined) ? 0 : ((array.reduce( ( prev, curr) => prev + curr, 0)) /array.length)
   }
+
+//Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+  function squareArea(A){
+    let area = ((2*A)/Math.PI)**2;
+    return Number(area.toFixed(2))
+    
+  }
