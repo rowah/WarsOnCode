c80ba19ea5e15ad1ae1af50ce34ddaa582cd8562
Rowa <jrowah@gmail.com>
2022-05-01 01:55:18 +0300

    Difference in volume of two cubes with measurements presented in two arrays

--- war30.js
+++ war30.js
@@ -69,3 +69,14 @@ function friend(friends){
     arr1.concat(arr2).forEach(el => !res.includes(el) ? res.push(el) : 0);
     return res.sort( (a, b) => a - b);
   }
+
+
+  //In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+  function findDifference(a, b) {
+    //loading...
+    return Math.abs((a[0]*a[1]*a[2]) - (b[0]*b[1]*b[2]))
+  }
+  //OR
+  function find_difference(a, b) {
+    return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
+  }
