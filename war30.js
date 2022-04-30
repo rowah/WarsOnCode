2e4a746a1fc1dc7bb7e9d6505142abd4659d3ee5
Rowa <jrowah@gmail.com>
2022-04-30 14:15:43 +0300

    Retruening the min and max values of in an array of numbers

--- war30.js
+++ war30.js
@@ -13,3 +13,14 @@ function getCount(str) {
     
     return vowelsCount;
   }
+
+//Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+//Task
+//Write a function that returns both the minimum and maximum number of the given list/array.
+  function minMax(arr){
+    arr.sort((a, b) => a-b)
+    return [arr[0],arr[arr.length-1]]; // fix me!
+  }
+  
+  minMax([2334454,5])
