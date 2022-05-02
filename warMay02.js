4da41d7fc3c7a7e1d2138f9a8907c3f9b1857d3a
Rowa <jrowah@gmail.com>
2022-05-03 01:30:25 +0300

    String reversal

--- warMay02.js
+++ warMay02.js
@@ -67,3 +67,11 @@ function bingo(a) {
  }
 return 'WIN'
   }
+
+
+  //Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+  function reverseWords(str) {
+    // Go for it
+    return str.split('').reverse().join('').split(' ').reverse().join(' ')
+  }
+  
