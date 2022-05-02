d1fa9d432968aa039b19c5101da52aa5ed9c1d61
Rowa <jrowah@gmail.com>
2022-05-03 01:02:19 +0300

    Bingo game

--- warMay02.js
+++ warMay02.js
@@ -51,3 +51,19 @@ function validatePIN (pin) {
 //For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
 
 //Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array). Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+const bingo = a => [2,7,9,14,15].every(e => a.includes(e)) ? 'WIN' : 'LOSE';
+
+function bingo(a) {
+    return [2,9,14,7,15].every(x => a.includes(x)) ? "WIN" : "LOSE"
+  }
+
+  function bingo(a) {
+    // your winning code here
+    let winningNos = [2,7,9,14,15]
+    for (let i = 0; i<winningNos.length; i++) {
+      if(!a.includes(winningNos[i])) {
+        return 'LOSE'
+  }
+ }
+return 'WIN'
+  }
