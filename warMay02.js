0822ac7777b99b51c1cb495688c12a5ab71e4c12
Rowa <jrowah@gmail.com>
2022-05-02 23:23:23 +0300

    pin validation

--- warMay02.js
+++ warMay02.js
@@ -21,3 +21,30 @@ function checkForFactor (base, factor) {
     // code here
     return (base % factor == 0) ? true : false
   }
+
+//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+//If the function is passed a valid PIN string, return true, else return false.
+
+function validatePIN (pin) {
+  
+    var pinlen = pin.length;
+    var isCorrectLength = (pinlen == 4 || pinlen == 6);
+    var hasOnlyNumbers = pin.match(/^\d+$/);
+      
+    if(isCorrectLength && hasOnlyNumbers){
+      return true;
+    }
+    
+    return false;
+  
+  }
+  //OR
+  var n = pin.length;
+  if( n != 4 && n != 6)
+      return false;
+  for (var i in pin)
+      if (pin[i] > '9' || pin[i] < '0')
+          return false;
+  return true;
+}
