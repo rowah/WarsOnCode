ea8550e6e1f15d2983ad17735b9340b75d6f80a4
Rowa <jrowah@gmail.com>
2022-05-02 22:46:15 +0300

    Checks factors for a base

--- warMay02.js
+++ warMay02.js
@@ -13,3 +13,11 @@ function hoopCount (n) {
    return "Keep at it until you get it"
  } else {return}
  }
+
+ //This function should test if the factor is a factor of base.
+
+//Return true if it is a factor or false if it is not.
+function checkForFactor (base, factor) {
+    // code here
+    return (base % factor == 0) ? true : false
+  }
