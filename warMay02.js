06c9fe0770cf34018917caa4c6d83be1426f2390
Rowa <jrowah@gmail.com>
2022-05-05 16:34:28 +0300

    knows indices of vowels in a word

--- warMay02.js
+++ warMay02.js
@@ -103,3 +103,29 @@ function getDivisorsCnt(n){
 }
 } return numOfDivisors
 }
+
+//We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+//So given a string "super", we should return a list of [2, 4].
+function vowelIndices(word){
+    const str = 'aeiouy';
+    const arr = word.toLowerCase().split('');
+    let answer = [];
+    arr.forEach((name, i) => {
+      if (str.indexOf(name) !== -1) {
+        answer.push(i + 1);
+      }
+    });
+    return answer;
+  }
+
+  //OR
+  function vowelIndices(word){
+    word = word.toLowerCase()
+    var ans = [];
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+    for(i=0; i<word.length; i++){
+      if(vowels.indexOf(word[i])>=0){ans.push(i+1)}
+    }
+    return ans
+  }
