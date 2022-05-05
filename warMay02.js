dd3203d7be34bd5c42e20e18a48e5c88fce6b0eb
Rowa <jrowah@gmail.com>
2022-05-05 17:09:42 +0300

    Returns numbers to string

--- warMay02.js
+++ warMay02.js
@@ -129,3 +129,21 @@ function vowelIndices(word){
     }
     return ans
   }
+
+  //All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+//Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+//Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+function feast(beast, dish) {
+    //your function here
+      return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+    }
+
+
+//We need a function that can transform a number into a string.
+//What ways of achieving this do you know?
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString()
+  }
