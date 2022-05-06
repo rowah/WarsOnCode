d3ac3083db23d9cf8876ac1ac52f4fb364206094
Rowa <jrowah@gmail.com>
2022-05-07 00:27:11 +0300

    String to number

--- warMay6.js
+++ warMay6.js
@@ -45,3 +45,21 @@ function shark(pDist, sDist, speedMe, speedShark, dolphin){
     
     return yourTime < sharksTime ? "Alive!": "Shark Bait!"
   }
+
+//We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+//Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+var stringToNumber = function(str){
+    // put your code here
+    return str *1
+  //   return null;
+  }
+  //OR
+  var stringToNumber = function(str){
+    return parseInt(str);
+  }
+
+  var stringToNumber = function(str){
+    // put your code here
+    return Number(str);
+  }
