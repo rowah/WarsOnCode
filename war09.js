688777effc14479ad3254d53803ed4c471b6a0ad
Rowa <jrowah@gmail.com>
2022-05-09 02:01:19 +0300

    Dont give me five

--- war09.js
+++ war09.js
@@ -29,3 +29,22 @@ function dontGiveMeFive(start, end){
   
   // dontGiveMeFive(1, 9)
   dontGiveMeFive(4,17)
+
+  //OR
+  function dontGiveMeFive(start, end){
+    let res = [];
+    for (let i = start; i <= end; i++) {
+      if (!i.toString().includes('5')) res.push(i);
+    }
+    return res.length;
+  }
+  //OR
+  function dontGiveMeFive(start, end) {
+    let count = 0
+    for (let i = start; i <= end; i++) {
+      if (!/5/.test(i)) {
+        count++
+      }
+    }
+    return count
+  }
